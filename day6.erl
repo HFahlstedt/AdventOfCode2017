73d7652a2e432632577ca26103a2e6f7f9e1c444
Håkan Fahlstedt <hf@fahlsoft.se>
2017-12-07 15:41:53 +0100

    Fixed Day 6

--- day6.erl
+++ day6.erl
@@ -1,27 +1,36 @@
 -module(day6).
 -export([solve_part1/0, solve_part2/0]).
 
-solve_part1() -> next_state(input()).
+solve_part1() -> solve(input()).
 
-solve_part2() -> [].
+solve_part2() -> {_, LoopStart} = solve_part1(), solve(LoopStart).
+
+solve(State) -> solve(State, sets:from_list([State])).
+
+solve(State, Previous) -> 
+    Next = next_state(0, first_max(State), lists:max(State), State),
+    case sets:is_element(Next, Previous) of 
+        true -> {sets:size(Previous), Next};
+        _Else -> solve(Next, sets:add_element(Next, Previous))
+    end.
 
 first_max(List) -> first_max(lists:max(List), 0, List).
 
 first_max(Max, Index, [Max|_]) -> Index;
 first_max(Max, Index, [_|Rest]) -> first_max(Max, Index+1, Rest).
 
-next_state(List) -> 
-    Max = lists:max(List), 
-    Length = length(List),
-    next_state(Max, first_max(List), Max div Length, (Max rem Length) - 1, Length, 0, List).
+next_state(_, _, _, []) -> [];
 
-next_state(_, _, _, _, Length, Length, _) -> [];
-next_state(Max, MaxIndex, PerCell, Overlapping, Length, MaxIndex, [Max|Rest]) -> 
-    [PerCell|next_state(Max, MaxIndex, PerCell, Overlapping, Length, MaxIndex+1, Rest)];
-next_state(Max, MaxIndex, PerCell, Overlapping, Length, Index, [V|Rest]) -> 
-    case Overlapping > (Index - MaxIndex - 1) rem (Length+1) of
-        true -> [V + PerCell + 1|next_state(Max, MaxIndex, PerCell, Overlapping, Length, Index+1, Rest)];
-        _Else -> [V + PerCell|next_state(Max, MaxIndex, PerCell, Overlapping, Length, Index+1, Rest)]
+next_state(MaxIndex, MaxIndex, Max, [_|Rest]) -> [Max div 16|next_state(MaxIndex+1, MaxIndex, Max, Rest)];
+
+
+next_state(Index, MaxIndex, Max, [Val|Rest]) -> 
+    case mod(Index - MaxIndex - 1, 16) < mod(Max-16, 16) of 
+        true -> [Val + (Max div 16) + 1|next_state(Index+1, MaxIndex, Max, Rest)];
+        _Else -> [Val + Max div 16|next_state(Index+1, MaxIndex, Max, Rest)]
     end.
+
+mod(N, D) when N rem D < 0 -> N rem D + D;
+mod(N, D) -> N rem D.
 
 input() -> [4, 10, 4, 1, 8, 4, 9, 14, 5, 1, 14, 15, 0, 15, 3, 5].
