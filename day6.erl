1e756c4b5aa31602f9327f23ee97234d1b461a6e
Håkan Fahlstedt <hf@fahlsoft.se>
2017-12-08 09:35:43 +0100

    Comments for Day 6

--- day6.erl
+++ day6.erl
@@ -1,6 +1,11 @@
 -module(day6).
 -export([solve_part1/0, solve_part2/0]).
 
+% Same solution for both parts, the difference is only in the input value, the input for part two is calculated in part one.
+% The first part gives the input for part two.
+% Calculate the next state by setting maximum to zero, adding Max div 16 to all cells and then adding one extra for the
+% Max mod 16 cells after maximum.
+
 solve_part1() -> solve(input()).
 
 solve_part2() -> {_, LoopStart} = solve_part1(), solve(LoopStart).
@@ -20,10 +25,7 @@ first_max(Max, Index, [Max|_]) -> Index;
 first_max(Max, Index, [_|Rest]) -> first_max(Max, Index+1, Rest).
 
 next_state(_, _, _, []) -> [];
-
 next_state(MaxIndex, MaxIndex, Max, [_|Rest]) -> [Max div 16|next_state(MaxIndex+1, MaxIndex, Max, Rest)];
-
-
 next_state(Index, MaxIndex, Max, [Val|Rest]) -> 
     case mod(Index - MaxIndex - 1, 16) < mod(Max-16, 16) of 
         true -> [Val + (Max div 16) + 1|next_state(Index+1, MaxIndex, Max, Rest)];
