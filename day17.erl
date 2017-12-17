5a36d827bf02884b7c46b2217121011fa4d58623
Håkan Fahlstedt <hf@fahlsoft.se>
2017-12-17 08:40:06 +0100

    Cleaned up Day 17

--- day17.erl
+++ day17.erl
@@ -5,17 +5,17 @@ solve_part1() -> solve(0, [0], 356, 1).
 solve_part2() -> solve2(0, 1, 356, 1, 0).
 
 solve(LastPos, Values, Steps, NextValue) -> 
-    NextPos = (LastPos + Steps) rem length(Values),
-    {A, B} = lists:split(NextPos+1, Values),
+    NextPos = (LastPos + Steps + 1) rem length(Values),
+    {A, B} = lists:split(NextPos, Values),
     case NextValue of
         2017 -> hd(B);
-        _ ->  solve((NextPos+1) rem (length(Values)+1), A ++ [NextValue] ++ B, Steps, NextValue+1)
+        _ ->  solve(NextPos, A ++ [NextValue] ++ B, Steps, NextValue+1)
     end.
 
 solve2(_, _, _, 50000000, AfterZero) -> AfterZero; 
 solve2(LastPos, ValueCount, Steps, NextValue, AfterZero) -> 
-    NextPos = (LastPos + Steps) rem ValueCount,
+    NextPos = (LastPos + Steps + 1) rem ValueCount,
     case NextPos of
-        0 -> solve2((NextPos+1) rem (ValueCount + 1), ValueCount+1, Steps, NextValue+1, NextValue);
-        _ -> solve2((NextPos+1) rem (ValueCount + 1), ValueCount+1, Steps, NextValue+1, AfterZero)
+        0 -> solve2(NextPos, ValueCount+1, Steps, NextValue+1, NextValue);
+        _ -> solve2(NextPos, ValueCount+1, Steps, NextValue+1, AfterZero)
     end.
